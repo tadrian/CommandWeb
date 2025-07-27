9addce84ed8e2e045ecd511926129257dda65f1e
Thomas Adrian <tadrian67@gmail.com>
2025-07-27 14:33:50 +0200

    Remove duplicate README.md from CommandWeb subfolder, keep root README.md as canonical

--- CommandWeb/obj/Debug/net10.0/win-x64/CommandWeb.AssemblyInfo.cs
+++ CommandWeb/obj/Debug/net10.0/win-x64/CommandWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommandWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+063e4a134e3c8aeaed2654c9cb353e228160b995")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommandWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommandWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
